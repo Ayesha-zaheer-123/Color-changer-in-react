0e6523d3333855ae5f06ff2a9068c8241f1b98fb
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-21 05:12:22 -0700

    add intial boilerplate code

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,5 +1,8 @@
 import './Background.css'
- function Background() {
-
+import { useState } from 'react'
+  export default function Background() {
+return(
+    <>
+    </>
+)
 }
-export default Background
